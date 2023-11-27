58d26fce43359e5d3616c26174568327065f48e6
MichaelMantovani <michmantovani@gmail.com>
2023-11-27 12:57:22 +0100

    test divide method

--- src/test/java/org/java/CalculatorTest.java
+++ src/test/java/org/java/CalculatorTest.java
@@ -8,7 +8,7 @@ import org.junit.jupiter.api.Test;
 public class CalculatorTest {
 	
 	private static Calculator c = null;
-	private static final float num1 = 2;
+	private static final float num1 = 6;
 	private static final float num2 = 3;
 
 	@BeforeAll
@@ -18,11 +18,16 @@ public class CalculatorTest {
 	
 	@Test
 	public void correctAdditionResultCalculatorTest() {
-		assertEquals(5, c.add(num1, num2), "La somma non è corretta");
+		assertEquals(9, c.add(num1, num2), "La somma non è corretta");
 	}
 	
 	@Test
 	public void correctSubtractionResultCalculatorTest() {
-		assertEquals(-1, c.subtract(num1, num2), "La sottrazione non  è corretta");
+		assertEquals(3, c.subtract(num1, num2), "La sottrazione non  è corretta");
+	}
+	
+	@Test
+	public void correctDivideResultCalculatorTest() {
+		assertEquals(2, c.divide(num1, num2)," La divisione non è corretta");
 	}
 }
